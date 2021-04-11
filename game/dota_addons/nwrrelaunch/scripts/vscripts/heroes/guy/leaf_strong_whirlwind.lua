9920fd6b65152f3873d09783746a46784fef8e85
LearningDave <38720791+XainDavidGroeger@users.noreply.github.com>
2021-04-11 19:59:22 +0200

    added guy 2nd spell animation

--- game/dota_addons/nwrrelaunch/scripts/vscripts/heroes/guy/leaf_strong_whirlwind.lua
+++ game/dota_addons/nwrrelaunch/scripts/vscripts/heroes/guy/leaf_strong_whirlwind.lua
@@ -1,5 +1,11 @@
 function playSound( keys )
-	-- body
+
+	if keys.ability:GetName() == "guy_leaf_strong_whirlwind_ult" then 
+		keys.caster:StartGestureWithPlaybackRate(ACT_DOTA_CAST_ABILITY_2_END, 3.0)
+	else
+		keys.caster:StartGestureWithPlaybackRate(ACT_DOTA_CAST_ABILITY_2_END, 1.0)
+	end
+
 end
 
 function effectStart( keys )
@@ -7,4 +13,6 @@ function effectStart( keys )
 	local particle_caster = ParticleManager:CreateParticle("particles/units/heroes/guy/senpuu_tornado.vpcf", PATTACH_ABSORIGIN, caster)
 	ParticleManager:SetParticleControl(particle_caster, 0, caster:GetAbsOrigin())
 	ParticleManager:SetParticleControl(particle_caster, 3, caster:GetAbsOrigin())
+
+	caster:RemoveGesture(ACT_DOTA_CAST_ABILITY_2_END)
 end
