97153fdfccc9846aeeed0b0faa3567d56a9b40f8
LearningDave <38720791+XainDavidGroeger@users.noreply.github.com>
2021-05-08 17:44:43 +0200

    added new vos for naruto temari and zabuza, started to rework zaubza

---
+++ game/dota_addons/nwrrelaunch/scripts/vscripts/heroes/zabuza/demon_of_the_hidden_mist.lua
@@ -0,0 +1,80 @@
+function markEnemy( keys )
+	local duration = keys.ability:GetLevelSpecialValueFor("duration", (keys.ability:GetLevel() - 1))
+	keys.ability:ApplyDataDrivenModifier(keys.caster,keys.target,"modifier_demon_mark",{duration = duration})
+	keys.ability.markedEnemy = keys.target
+end
+
+function checkDistance( keys )
+	local distance = (keys.ability.markedEnemy:GetAbsOrigin() - keys.caster:GetAbsOrigin()):Length2D()
+
+	if distance >= 1500 or not keys.ability.markedEnemy:IsAlive() then
+		keys.caster:RemoveModifierByName("modifier_demon_unkillable")
+		keys.ability.markedEnemy:RemoveModifierByName("modifier_demon_mark")
+	end
+
+end
+
+
+function registerDamage( keys )
+
+	local target = keys.unit
+	local damageTaken = keys.DamageTaken
+	if not target.demon_damage then
+		target.demon_damage = 0
+	end
+	keys.caster:SetHealth(keys.caster:GetHealth() + damageTaken)
+	
+	target.demon_damage = target.demon_damage + damageTaken
+	if keys.attacker:IsRealHero() then
+		target.lastAttacker = keys.attacker
+	end
+	if target.lastAttacker == nil then
+		target.lastAttacker = keys.attacker
+	end 
+	
+end
+
+function spreadDamage( keys )
+	-- Init in case never take any damage
+	if not keys.target.demon_damage then
+		keys.target.demon_damage = 0
+	end
+
+	-- Variables
+	local target = keys.target
+	local ability = keys.ability
+	local minimumHealth = 1
+	if (target:GetHealth() - target.demon_damage) < 1 then
+		if not target.lastAttacker:IsRealHero() then
+			if target.lastAttackerResort ~= nil then
+				target:Kill(ability, target.lastAttackerResort)
+			else
+				target:Kill(ability, target.lastAttacker)
+			end
+		else
+			target:Kill(ability, target.lastAttacker)
+		end
+		
+	else
+		target:SetHealth( target:GetHealth() - target.demon_damage )
+	end
+	target.demon_damage = 0
+	target.lastAttackerResort = nil
+	target.lastAttacker = nil
+end
+
+
+function registerAttacker( keys )
+	if keys.attacker:IsRealHero() then
+		keys.caster.lastAttackerResort = keys.attacker
+	end
+end
+
+function playSound( keys )
+	local random = math.random(1, 2)
+	if random == 1 then
+		EmitSoundOn("zabuza_ult",keys.caster)
+	elseif random == 2 then
+		EmitSoundOn("zabuza_ult_2",keys.caster)
+	end
+end
--- game/dota_addons/nwrrelaunch/scripts/vscripts/heroes/zabuza/kirigakure_no_jutsu.lua
+++ game/dota_addons/nwrrelaunch/scripts/vscripts/heroes/zabuza/kirigakure_no_jutsu.lua
@@ -1,9 +1,6 @@
 LinkLuaModifier("modifier_chronosphere_speed_lua", "heroes/hero_faceless_void/modifiers/modifier_chronosphere_speed_lua.lua", LUA_MODIFIER_MOTION_NONE)
 
---[[Author: LearningDave
-	Date: 22.10.2015
-	Creates a dummy at the target location that acts as the Fog
-	]]
+
 function Chronosphere( keys )
 	-- Variables
 	local caster = keys.caster
@@ -25,111 +22,93 @@ function Chronosphere( keys )
 end
 
 
-function applySmokeModifier( keys )
-
-	local ability = keys.ability
-	local caster = keys.caster
-	local target = keys.target
-
-	local duration = 1
-
-
-	local modifier_name = "modifier_zabuza_kirigakure_no_jutsu_silence"
-
-
-	local ability2 = caster:FindAbilityByName("special_bonus_zabuza_2")
-	if ability2 ~= nil then
-		if ability2:IsTrained() then
-			modifier_name = "modifier_zabuza_kirigakure_no_jutsu_silence_special"
-		end
+function applyInvis( keys )
+	local duration = keys.ability:GetLevelSpecialValueFor("duration", (keys.ability:GetLevel() - 1))
+	if not keys.caster:HasModifier("modifier_zabuza_kirigakure_no_jutsu_ms_buff") then
+		keys.ability:ApplyDataDrivenModifier(keys.caster,keys.target,"modifier_zabuza_kirigakure_no_jutsu_ms_buff",{duration = duration})
 	end
-	
-
-	keys.ability:ApplyDataDrivenModifier(caster,target,modifier_name, 
-		{
-			duration = duration
-		}
-	)
-end
-
-function applySmokeModifierNew( keys )
-
-	local ability = keys.ability
-	local caster = keys.caster
-	local target = keys.target
-	local radius = ability:GetLevelSpecialValueFor("radius", ability:GetLevel() - 1)
-
-
-	local ability4 = caster:FindAbilityByName("special_bonus_zabuza_4")
-	if ability4 ~= nil then
-		if ability4:IsTrained() then
-			radius = radius + 175
-		end
-	end
-	
-
-	local targets = FindUnitsInRadius(
-		caster:GetTeamNumber(), 
-		target:GetAbsOrigin(), 
-		nil, 
-		radius, 
-		DOTA_UNIT_TARGET_TEAM_ENEMY, 
-		DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, 
-		0, 
-		0, 
-		false
-	)
-
-	local duration = 1
-
-
-	local modifier_name = "modifier_zabuza_kirigakure_no_jutsu_silence"
-
-	local ability2 = caster:FindAbilityByName("special_bonus_zabuza_2")
-	if ability2 ~= nil then
-		if ability2:IsTrained() then
-			modifier_name = "modifier_zabuza_kirigakure_no_jutsu_silence_special"
-		end
-	end
-	
-
-	for _, unit in pairs(targets) do
-		keys.ability:ApplyDataDrivenModifier(caster,unit,modifier_name, 
-			{
-				duration = duration
-			}
-		)
-	end
-	
 end
 
 
-function attachEffect( keys )
+function spin_web( keys )
 
-	local ability = keys.ability
 	local caster = keys.caster
-	local target = keys.target
 
-	local radius = ability:GetLevelSpecialValueFor("radius", ability:GetLevel() - 1)
-	local duration = ability:GetLevelSpecialValueFor("duration", ability:GetLevel() - 1)
-	local ability4 = caster:FindAbilityByName("special_bonus_zabuza_4")
-	if ability4 ~= nil then
-		if ability4:IsTrained() then
-			radius = radius + 175
-		end
-	end
-	
+		-- Variables
+		local target = keys.target_points[1]
+		local ability = keys.ability
+		local player = caster:GetPlayerID()
 
-	local smoke_particle = "particles/units/heroes/hero_riki/riki_smokebomb.vpcf"
-	local particle = ParticleManager:CreateParticle(smoke_particle, PATTACH_WORLDORIGIN, keys.target)
-	ParticleManager:SetParticleControl(particle, 0, keys.target:GetAbsOrigin())
-	ParticleManager:SetParticleControl(particle, 1, Vector(radius, 0, radius))
+		-- Modifiers and dummy abilities/modifiers
+		local stack_modifier = keys.stack_modifier
+		local dummy_modifier = keys.dummy_modifier
 
-	Timers:CreateTimer(duration, function()
-		ParticleManager:DestroyParticle(particle, false)
-		ParticleManager:ReleaseParticleIndex(particle)
-	end)
+		-- AbilitySpecial variables
+		local maximum_charges = ability:GetLevelSpecialValueFor( "max_charges", ( ability:GetLevel() - 1 ) )
+		local duration = ability:GetLevelSpecialValueFor( "duration", ( ability:GetLevel() - 1 ) )
+		local charge_replenish_time = ability:GetLevelSpecialValueFor( "charge_restore_time", ( ability:GetLevel() - 1 ) )
 
+		-- Dummy
+		local dummy = CreateUnitByName("npc_dummy_unit", target, false, caster, caster, caster:GetTeam())
+		ability:ApplyDataDrivenModifier(caster, dummy, dummy_modifier, {})
+		dummy:SetControllableByPlayer(player, true)
+		
+
+
+		-- Timer to remove the dummy
+		Timers:CreateTimer(duration, function() dummy:RemoveSelf() end)
 end
 
 
+function spin_web_aura( keys )
+	local ability = keys.ability
+	local caster = keys.caster	
+	local target = keys.target
+
+	-- Owner variables
+	local caster_owner = caster:GetPlayerOwner()
+	local target_owner = target:GetPlayerOwner()
+
+	-- Units
+	local unit_spiderling = keys.unit_spiderling
+	local unit_spiderite = keys.unit_spiderite
+	local all_units = ability:GetLevelSpecialValueFor("all_units", (ability:GetLevel() - 1))
+
+	-- Modifiers
+	local aura_modifier = keys.aura_modifier
+	local pathing_modifier = keys.pathing_modifier
+	local pathing_fade_modifier = keys.pathing_fade_modifier
+	local invis_modifier = keys.invis_modifier
+	local invis_fade_modifier = keys.invis_fade_modifier
+
+
+	if keys.target:IsRealHero() then
+
+
+		ability:ApplyDataDrivenModifier(caster, target, aura_modifier, {})
+
+				-- If it doesnt have the fade pathing modifier or the pathing modifier then apply it
+		if not target:HasModifier(pathing_fade_modifier) and not target:HasModifier(pathing_modifier) then
+			ability:ApplyDataDrivenModifier(caster, target, pathing_modifier, {}) 
+		end
+
+				-- If it doesnt have the fade invis modifier or the invis modifier then apply it
+		if not target:HasModifier(invis_modifier) and not target:HasModifier(invis_fade_modifier) then
+	--		ability:ApplyDataDrivenModifier(caster, target, invis_fade_modifier, {})
+		end
+	end
+end
+
+function appylMsBoost( keys )
+	local ability = keys.ability
+	local caster = keys.caster	
+	local target = keys.target
+
+	-- Owner variables
+	local caster_owner = caster:GetPlayerOwner()
+	local target_owner = target:GetPlayerOwner()
+	
+	if caster_owner == target_owner then
+		keys.ability:ApplyDataDrivenModifier(keys.caster, keys.target, keys.ms_modifier, {})
+	end
+end
