102dafb9ac83556ebb2db5caeaa1d8182d633458
DigitalG <thesever99@gmail.com>
2021-05-14 13:50:41 +0300

    merge

--- game/dota_addons/nwrrelaunch/scripts/vscripts/heroes/temari/fusajin_no_jutsu.lua
+++ game/dota_addons/nwrrelaunch/scripts/vscripts/heroes/temari/fusajin_no_jutsu.lua
@@ -1,60 +1,94 @@
-function CanBeReflected(bool, target, ability)
-    if bool == true then
-        if target:TriggerSpellReflect(ability) then return end
-    else
-        --[[ simulate the cancellation of the ability if it is not reflected ]]
-ParticleManager:CreateParticle("particles/items3_fx/lotus_orb_reflect.vpcf", PATTACH_ABSORIGIN, target)
-        EmitSoundOn("DOTA_Item.AbyssalBlade.Activate", target)
-    end
-end
+-- function applyDamage ( keys )
 
-function applyDamage ( keys )
+-- 	local ability = keys.ability
+-- 	local caster = keys.caster
+  
+-- 	local damage = ability:GetLevelSpecialValueFor("damage", (ability:GetLevel() - 1))
+  
+-- 	local ability1 = caster:FindAbilityByName("special_bonus_temari_1")
+	
+-- 	if ability1:IsTrained() then
+-- 		damage = damage + 75
+-- 	end
+  
+-- 	local damage_table = {
+-- 	  victim = keys.target,
+-- 	  attacker = keys.caster,
+-- 	  damage = damage,
+-- 	  damage_type = DAMAGE_TYPE_MAGICAL,		
+-- 	  ability = keys.abiltiy
+-- 	}
+  
+-- 	ApplyDamage( damage_table )
 
-	local ability = keys.ability
-	local caster = keys.caster
-	local target = keys.target
-	
-	--[[ if the target used Lotus Orb, reflects the ability back into the caster ]]
-    if target:FindModifierByName("modifier_item_lotus_orb_active") then
-        CanBeReflected(false, target, ability)
-        
-        return
-    end
-    
-    --[[ if the target has Linken's Sphere, cancels the use of the ability ]]
-    if target:TriggerSpellAbsorb(ability) then return end
-  
-	local damage = ability:GetLevelSpecialValueFor("damage", (ability:GetLevel() - 1))
-  
-	local ability1 = caster:FindAbilityByName("special_bonus_temari_1")
-	
-	if ability1 ~= nil then
-	    if ability1:IsTrained() then
-	    	damage = damage + 75
-	    end
-	end
-  
-	local damage_table = {
-	  victim = keys.target,
-	  attacker = keys.caster,
-	  damage = damage,
-	  damage_type = DAMAGE_TYPE_MAGICAL,		
-	  ability = keys.abiltiy
+-- end
+
+
+-- function resetCooldown( keys )
+
+-- 	local ability2 = keys.caster:FindAbilityByName("special_bonus_temari_2")
+-- 	if ability2:IsTrained() then
+-- 		keys.ability:EndCooldown()
+-- 		keys.ability:StartCooldown(keys.ability:GetCooldown(keys.ability:GetLevel() - 1) - 2)
+-- 	end
+
+-- end
+
+
+temari_fusajin_no_jutsu = class({})
+
+function temari_fusajin_no_jutsu:OnSpellStart()
+	local caster = self:GetCaster()
+	local projectile_data = {
+		EffectName = "particles/units/heroes/temari/temari_wind_dust.vpcf",
+		Ability = self,
+		iMoveSpeed = self:GetSpecialValueFor("magic_missile_speed"),
+		Source = caster,
+		Target = self:GetCursorTarget(),
+		bDodgeable = false,
+		iSourceAttachment = "attach_right_hand",
+		bProvidesVision = false,
+		iVisionTeamNumber = caster:GetTeamNumber(),
+		iVisionRadius = 0,
 	}
-  
-	ApplyDamage( damage_table )
+
+	ProjectileManager:CreateTrackingProjectile(projectile_data)
+
+	local cd_reduction = self:GetSpecialValueFor("cooldown_reduction_other_abilities")
+	local kamaitachi_ability = caster:FindAbilityByName("temari_kamaitachi_no_jutsu")
+	if kamaitachi_ability:IsTrained() and not kamaitachi_ability:IsCooldownReady() then
+		local left = kamaitachi_ability:GetCooldownTimeRemaining()
+		kamaitachi_ability:EndCooldown()
+		kamaitachi_ability:StartCooldown(left - cd_reduction)
+	end
+
+
+	local kiri_ability = caster:FindAbilityByName("temari_kuchiyose_kirikiri_mai")
+	if kiri_ability:IsTrained() and not kiri_ability:IsCooldownReady() then
+		local left = kiri_ability:GetCooldownTimeRemaining()
+		kiri_ability:EndCooldown()
+		kiri_ability:StartCooldown(left - cd_reduction)
+	end
 
 end
 
+function temari_fusajin_no_jutsu:OnProjectileHit(target, location)
+	local damage_table = {
+		victim = target,
+		attacker = self:GetCaster(),
+		damage = self:GetSpecialValueFor("damage"),
+		damage_type = self:GetAbilityDamageType(),
+		damage_flags = DOTA_DAMAGE_FLAG_NONE,
+		ability = self
+	}
 
-function resetCooldown( keys )
-
-	local ability2 = keys.caster:FindAbilityByName("special_bonus_temari_2")
-	if ability2 ~= nil then
-	    if ability2:IsTrained() then
-	    	keys.ability:EndCooldown()
-	    	keys.ability:StartCooldown(keys.ability:GetCooldown(keys.ability:GetLevel() - 1) - 2)
-	    end
-    end
+	ApplyDamage(damage_table)
 end
 
+function temari_fusajin_no_jutsu:GetCooldown()
+	if self:GetCaster():FindAbilityByName("special_bonus_temari_2"):GetLevel() > 0 then
+		return 3
+	else
+		return 5
+	end
+end
