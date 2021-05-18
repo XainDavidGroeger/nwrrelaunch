35ed772bb82b6d54587bdff2bd78d5571505ddcc
spastic <ryaninwards45@gmail.com>
2021-05-18 09:51:17 -0500

    some bullshit that wasn't compiled and Sasuke SFX stuff

--- game/dota_addons/nwrrelaunch/scripts/vscripts/heroes/sasuke/sasuke_chidori_stream.lua
+++ game/dota_addons/nwrrelaunch/scripts/vscripts/heroes/sasuke/sasuke_chidori_stream.lua
@@ -30,6 +30,7 @@ function sasuke_chidori_stream:OnSpellStart()
 	ParticleManager:SetParticleControl( self.stream, 3, Vector(self:GetSpecialValueFor("radius") + self:GetCaster():FindTalentValue("special_bonus_sasuke_5")  ,0,0) )
 
 	self:GetCaster():EmitSound("sasuke_stream_cast")
+	self:GetCaster():EmitSound("sasuke_stream_talking")
 
 	local targets = FindUnitsInRadius(self:GetCaster():GetTeam(), self:GetCaster():GetAbsOrigin(), nil, 
 		self:GetSpecialValueFor("radius") + self:GetCaster():FindTalentValue("special_bonus_sasuke_5"), DOTA_UNIT_TARGET_TEAM_ENEMY,
